1002703b2501429dcdd4cbeeea3689bd3cf6a0e4
jin4333 <vhfhflsksk@gmail.com>
2023-03-14 16:15:14 +0900

    first commit

---
+++ src/components/HeaderDisplay.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h2>weather app</h2>
+    <p>{{ username }}님 반갑습니다!</p>
+  </header>
+</template>
+
+<script>
+export default {
+  username: "jin",
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background: linear-gradient(to right, #ee9ca7, #ffdde1);
+}
+.header h2 {
+  color: #fff;
+}
+</style>
---
+++ src/components/weather/ContentHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="content-header">
+    <h2>날씨를 알려드리겠습니다.</h2>
+    <p>원하시는 도시를 선택해 주세요.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentHeader",
+};
+</script>
+
+<style scoped>
+.content-header {
+  padding: 1rem;
+}
+.content-header p {
+  font-size: 14px;
+}
+</style>
